f1d9901229104b9d16ef0b5f16d968dfdf484f23
burningserenity <burningserenity@bitmai.la>
2017-11-14 19:57:17 -0500

    Installed more packages and created server.js boilerplate

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+
+// Express configuration
+const app = express();
+// Heroku uses Heroku's port, the default Lisk testnet port is 7000
+const port = process.env.PORT || 7000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+app.use(methodOverride("_method");
+
+app.listen(port) {
+    console.log(`Listening on port ${port}...`);
+}
