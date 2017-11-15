20e8920896de4b3b7cf490fc449411a56bd58e6d
burningserenity <burningserenity@bitmai.la>
2017-11-14 23:27:53 -0500

    Working seeds, connects to JSON array at root path

--- db/voterseeds.sql
+++ db/voterseeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO Voter (voter_address, voter_username)
-    VALUES ("1234567890L", "user1");
+INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
+    VALUES ('1234567890L', 'user1', now(), now());
 
-INSERT INTO Voter (voter_address, voter_username)
-    VALUES ("2345678901L", "user2");
+INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
+    VALUES ('2345678901L', 'user2', now(), now());
 
-INSERT INTO Voter (voter_address, voter_username)
-    VALUES ("3456789012L", "user3");
+INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
+    VALUES ('3456789012L', 'user3', now(), now());
 
 
