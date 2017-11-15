259a29d165b03f860e640cf0186a78b0a76a95f2
burningserenity <burningserenity@bitmai.la>
2017-11-15 12:52:21 -0500

    Added basic crud for voters, need to create way to generate address and encrypted addresses

--- db/voterseeds.sql
+++ db/voterseeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
-    VALUES ('1234567890L', 'user1', now(), now());
+INSERT INTO "Voters" (voter_address, voter_passphrase, "createdAt", "updatedAt")
+    VALUES ('1234567890L', 'foo bar baz buz boing ping lisk', now(), now());
 
-INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
-    VALUES ('2345678901L', 'user2', now(), now());
+INSERT INTO "Voters" (voter_address, voter_passphrase, "createdAt", "updatedAt")
+    VALUES ('2345678901L', 'bar baz buz boing ping lisk foo', now(), now());
 
-INSERT INTO "Voters" (voter_address, voter_username, "createdAt", "updatedAt")
-    VALUES ('3456789012L', 'user3', now(), now());
+INSERT INTO "Voters" (voter_address, voter_passphrase, "createdAt", "updatedAt")
+    VALUES ('3456789012L', 'baz buz boing ping lisk foo bar', now(), now());
 
 
--- routes/voter_routes.js
+++ routes/voter_routes.js
@@ -4,9 +4,96 @@ const db = require('../models');
 const router = express.Router();
 const voter = db.Voter;
 
+/* Root */
+
 router.get("/", (req, res) => {
     voter.findAll().then(dbVoter => {
         console.log(dbVoter);
+        res.json(dbVoter);
+    });
+});
+
+
+/* API */
+
+// Select all voters
+router.get("/api/voters", (req, res) => {
+    voter.findAll().then(dbVoter => {
+        console.log(dbVoter);
+        res.json(dbVoter);
+    });
+});
+
+// Select a single voter by address
+router.get("/api/voters/:address", (req, res) => {
+    voter.findOne({
+        where: {
+            voter_address: req.params.address
+        }
+    }).then(dbVoter => {
+        console.log(dbVoter);
+        res.json(dbVoter);
+    });
+});
+
+// Add a voter
+router.post("/api/voters", (req, res) => {
+    console.log(req.body);
+    voter.create({
+        voter_passphrase: req.body.passhprase,
+        voter_firstName: req.body.firstName,
+        voter_lastName: req.body.lastName,
+        voter_email: req.body.email
+    }).then(dbVoter => {
+        res.json(dbVoter);
+    });
+});
+
+// Delete a voter
+router.delete("/api/voters/:address", (req, res) => {
+    voter.destroy({
+        where: {
+            voter_address: req.params.address
+        }
+    }).then(dbVoter => {
+        res.json(dbVoter);
+    });
+});
+
+// Update a voter ---- This is only for updating first/last name, email, and/or username
+router.put("/api/voters", (req, res) => {
+    voter.update(req.body,
+        {
+            where: {
+                address: req.body.address
+            }
+        }).then(dbVoter => {
+            res.json(dbVoter);
+    });
+});
+
+// Change a voter's stake
+router.put("/api/voters/stake/:address", (req, res) => {
+    voter.update({
+        voter_stake: req.body.stake,
+    },{
+        where: {
+            voter_address: req.params.address
+        }
+    }).then(dbVoter => {
+        res.json(dbVoter);
+    });
+});
+
+// Anonymise user per ballot
+router.put("/api/voters/anon/:address", (req, res) => {
+    voter.update({
+        voter_currenthash: req.body.currenthash
+    }, {
+        where: {
+            voter_address: req.params.address
+        }
+    }).then(dbVoter => {
         res.json(dbVoter);
     });
 });
