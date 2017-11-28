f541c7683bc4a0644303deca9daf490d318fae97
burningserenity <burningserenity@bitmai.la>
2017-11-28 00:27:28 -0500

    Working on the component

--- client/src/components/BallotCards/BallotCards.js
+++ client/src/components/BallotCards/BallotCards.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ListGroup, ListGroupItem, Table, Panel, FormGroup, Radio } from 'react-bootstrap';
 
-const BallotCard = props => 
-    <div>
-        <h2>{props.ballot[0].ballot_name}</h2>
-    <p>{JSON.stringify(props.issues[1].issue_name, null, 2)}</p>
-        <div>
-        </div>
-    </div>;
+class BallotCard extends Component  {
+    constructor(props) {
+        super(props);
+
+        handleChange(e) {
+            let prop = e.target.id;
+            let change = {};
+            change [prop] = e.target.value;
+            this.setState(change);
+        }
+    }
+    render() {
+        return (
+            <div>
+                {props.ballot[0].Issues.map( issue => (
+                    <Panel defaultExpanded header={props.ballot[0].ballot_name}>
+                        {issue.issue_name}
+                        <FormGroup fill>
+                            {issue.Positions.map( position => (
+                                <Radio name="radioGroup" onchange={this.handleChange.bind(this)} id={position.position_id}>{position.position_name}</Radio>
+                            ))}
+                        </FormGroup>
+                        Expires: {props.ballot[0].ballot_expiration}
+                    </Panel>
+                ))}
+            </div>;
+        )
+    }
+}
 
 export default BallotCard;
