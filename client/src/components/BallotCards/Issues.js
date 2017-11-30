c23ce7e1322d4699169322d47e84ecda5f8d8ea7
Allison Marie <aliederman@icloud.com>
2017-11-30 12:33:01 -0500

    styling the ballotcard

--- client/src/components/BallotCards/Issues.js
+++ client/src/components/BallotCards/Issues.js
@@ -4,8 +4,8 @@ import { Panel, FormGroup, Radio } from 'react-bootstrap';
 const Issue = props =>
     <div>
         {props.issue &&
-            <Panel defaultExpanded header={props.ballot_name}>
-                {props.issue.issue_name}
+            <Panel header={props.ballot_name} bsStyle="primary">
+                <strong>Issue Title: </strong>{props.issue.issue_name}
                 <FormGroup fill>
                     {props.issue.Positions.map(position => (
                         <Radio key={position.id} name={`radioGroup-${props.issue.id}`} onClick={props.onClick.bind(this)} id={`p-${position.id}`} value={`${props.issue.id},${position.id}`}>{position.position_name}</Radio>
